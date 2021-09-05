d225ce36a07d959064b03ff69349017d65adc129
midxdle <sina.heydarian.sh@gmail.com>
2021-09-05 15:27:12 +0430

    index now has a function to check if user is log in or no then redirect user to login page or books page

--- controllers/index.js
+++ controllers/index.js
@@ -4,23 +4,28 @@ var Book = require('../models/bookModel');
 
 
 module.exports = function (router) {
-    router.get('/', function (req, res) {
+    router.get('/', ensureAuthenticated, function (req, res) { 
         Book.find({}, function(err, books){
             if(err){
                 console.log(err);
             } 
-
+    
             books.forEach(function(book){
                 book.truncText = book.truncText(50);
             });
-
+    
             var model = {
                 books: books
             }
-
             res.render('index', model);
         });
-        
     });
+
+    function ensureAuthenticated(req, res, next) {
+        if(req.isAuthenticated()) {
+          return next();
+        } 
+        res.redirect('/users/login');
+      }
 
 };
