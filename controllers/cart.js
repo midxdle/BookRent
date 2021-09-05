eb5c3524a414adc40a185c9fff29c2c132d67010
midxdle <sina.heydarian.sh@gmail.com>
2021-09-05 15:25:21 +0430

    now users option added and you have to first register and then login to rent a book

--- controllers/cart.js
+++ controllers/cart.js
@@ -5,51 +5,6 @@ var Category = require('../models/categoryModel');
 var Rent = require('../models/rentModel');
 
 module.exports = function(router) {
-    // router.get('/', function(req, res) {
-    //     var cart = req.session.cart;
-    //     var displayCart = {items: [], total:0};
-    //     var total = 0;
-
-    //     for(var item in cart) {
-    //         displayCart.items.push(cart[item]);
-    //         total += (cart[item].qty * cart[item].price);
-
-    //     }
-    //     displayCart.total = total;
-
-    //     res.render('cart/index', {
-    //         cart : displayCart
-    //     });
-    // });
-
-    // router.post('/:id', function(req, res) {
-    //     req.session.cart = req.session.cart || {};
-    //     var cart = req.session.cart;
-
-    //     Book.findOne({_id : req.params.id}, function(err, book) {
-    //         if(err) {
-    //             console.log(err);
-    //         }
-
-    //         if(cart[req.params.id]) {
-    //             cart[req.params.id].qty++
-    //         } else {
-    //             cart[req.params.id] = {
-    //                 item : book._id,
-    //                 title : book.title,
-    //                 price : book.price,
-    //                 qty : 1
-    //             }
-    //         }
-
-    //         res.redirect('/cart');
-    //     });
-    // });
-
-    // router.get('/remove', function(req, res) {
-    //     req.session.destroy();
-    //     res.redirect('/cart');
-    // });
 
     router.get('/', function(req, res) {
         Book.find({},{}, function(err, books){
@@ -100,6 +55,52 @@ module.exports = function(router) {
             });
         });
     });
+
+    // router.get('/', function(req, res) {
+    //     var cart = req.session.cart;
+    //     var displayCart = {items: [], total:0};
+    //     var total = 0;
+
+    //     for(var item in cart) {
+    //         displayCart.items.push(cart[item]);
+    //         total += (cart[item].qty * cart[item].price);
+
+    //     }
+    //     displayCart.total = total;
+
+    //     res.render('cart/index', {
+    //         cart : displayCart
+    //     });
+    // });
+
+    // router.post('/:id', function(req, res) {
+    //     req.session.cart = req.session.cart || {};
+    //     var cart = req.session.cart;
+
+    //     Book.findOne({_id : req.params.id}, function(err, book) {
+    //         if(err) {
+    //             console.log(err);
+    //         }
+
+    //         if(cart[req.params.id]) {
+    //             cart[req.params.id].qty++
+    //         } else {
+    //             cart[req.params.id] = {
+    //                 item : book._id,
+    //                 title : book.title,
+    //                 price : book.price,
+    //                 qty : 1
+    //             }
+    //         }
+
+    //         res.redirect('/cart');
+    //     });
+    // });
+
+    // router.get('/remove', function(req, res) {
+    //     req.session.destroy();
+    //     res.redirect('/cart');
+    // });
 }
 
 // router.post('/rent/:id', function(req, res) {
